cdc4895abedad49e4a4a116dd349a2562eb34a5a
benito <greg39483@gmail.com>
2026-05-16 04:23:30 -0400

    test pipeline

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-print ("Secure cloud app running")
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+
+@app.route("/")
+def home():
+    app.logger.info("Homepage accessed")
+    return "Secure Cloud App Running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
+
+import os
+
+secret = os.gentnv("APP_MODE")
+
+print(secret)
+
+from flask_talisman import Talisman
+
+Talisman(app)
+
+@app.route("/health")
+def health():
+    return {"status":"healthy"}
